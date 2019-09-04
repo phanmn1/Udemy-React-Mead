9f52b6eea1de565ec8aee31147700f3252f9eaf3
Minh Phan <mpflamingmarshmallow@gmail.com>
2019-09-04 13:01:01 -0700

    Styled React App minus modal window

--- src/app.js
+++ src/app.js
@@ -1,47 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import IndecisionApp from './components/IndecisionApp'
-
+import './styles/styles.scss'
+import 'normalize.css/normalize.css'
 
 ReactDOM.render(<IndecisionApp options={['Option1', 'Options 2']}/>, document.getElementById('app'))
 
-// const Layout = (props) => {
-//     return (
-//         <div>
-//             <p>Header</p>
-//             {props.children}
-//             <p>Footer</p>
-//         </div>
-//     )
-// }
-
-// const template = (
-//     <div>
-//         <h1>Page Title</h1>
-//         <p>This is my page</p>
-//     </div>
-// )
-
-// ReactDOM.render(<Layout><p>This is inline</p></Layout>, document.getElementById('app'))
-
-// class OldSyntax {
-//     constructor() {
-//         this.name = "Mike"
-//     }
-// }
-
-// const oldSyntax = new OldSyntax();
-// console.log(oldSyntax);
-
-// // -------------
-// class NewSyntax {
-//     name = 'Jen';
-//     getGreeting = () => {
-//         return `Hi. My name is ${this.name}.`;
-//     }
-    
-// }
-
-// const newSyntax = new NewSyntax(); 
-// const newGetGreeting = newSyntax.getGreeting;
-// console.log(newGetGreeting())
--- src/components/Options.js
+++ src/components/Options.js
@@ -5,18 +5,28 @@ const Options = (props) =>
     (
             
         <div>
-        <button onClick={props.handleDeleteOptions}>remove All</button>
-            {
-                props.options.map((element) => (
-                    <Option 
-                        key={element} 
-                        optionText={element}
-                        handleDeleteOption={props.handleDeleteOption}
-                        />
-                    
-                    ))
-            }
-        
+            <div className="widget-header">
+                <h3 className="widget-header__title">Your Options</h3>
+                <button 
+                    onClick={props.handleDeleteOptions}
+                    className="button button--link"
+                    >
+                    Remove All
+                    </button>
+                </div>
+                {props.options.length === 0 && <p className="message">Please add option to get started!</p>}
+                {
+                    props.options.map((element, index) => (
+                        <Option 
+                            key={element} 
+                            optionText={element}
+                            count={index+1}
+                            handleDeleteOption={props.handleDeleteOption}
+                            />
+                        
+                        ))
+                }
+            
         </div>
     ) 
 
